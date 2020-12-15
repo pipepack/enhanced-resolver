0480e72136998f9b989c1c5b628e7e90a0d39750
huang.jian <hjj491229492@hotmail.com>
2020-12-15 17:24:19 +0800

    refactor: drop unnecessary exports

--- src/index.ts
+++ src/index.ts
@@ -8,18 +8,10 @@ export { Resolver } from './Resolver';
 export { AssetResolver } from './AssetResolver';
 export { NodeResolver } from './NodeResolver';
 
-// built-in operator
-export { parse } from './operators/parse';
-export { possible } from './operators/possible';
-export { relativize } from './operators/relativize';
-export { replenish } from './operators/replenish';
-export { probe } from './operators/probe';
-
 // built-in interface
 export * from './interface/fs';
 export * from './interface/resolver';
 
 // built-in utils
-export { assign } from './utils/assign';
-export { promisify } from './utils/promisify';
-export { Channel } from './utils/constant';
+export * from './utils/constant';
+export * from './utils/promisify';
